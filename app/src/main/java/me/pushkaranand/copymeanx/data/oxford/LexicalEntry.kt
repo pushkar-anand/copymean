db99d45d3e0697decb4dca2a44cb524f17fe891d
Pushkar Anand <anandpushkar088@gmail.com>
2019-08-15 18:22:42 +0530

    added objects for api response.

---
+++ app/src/main/java/me/pushkaranand/copymeanx/data/oxford/LexicalEntry.kt
@@ -0,0 +1,6 @@
+package me.pushkaranand.copymeanx.data.oxford
+
+class LexicalEntry(
+
+) {
+}
---
+++ app/src/main/java/me/pushkaranand/copymeanx/data/oxford/MetaData.kt
@@ -0,0 +1,7 @@
+package me.pushkaranand.copymeanx.data.oxford
+
+data class MetaData(
+    val operation: String,
+    val provider: String,
+    val schema: String
+)
