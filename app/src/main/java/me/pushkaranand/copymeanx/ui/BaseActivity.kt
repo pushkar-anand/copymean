cc48c1982291081dd0208e33a2ce8791c5fcbfab
Pushkar Anand <anandpushkar088@gmail.com>
2019-10-29 15:05:21 +0530

    Removed MainActivity and replaced with BaseActivity as launcher.

--- app/src/main/java/me/pushkaranand/copymeanx/ui/BaseActivity.kt
+++ app/src/main/java/me/pushkaranand/copymeanx/ui/BaseActivity.kt
@@ -1,13 +1,30 @@
 package me.pushkaranand.copymeanx.ui
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import me.pushkaranand.copymeanx.R
+import me.pushkaranand.copymeanx.utils.FirstRunHelper
 
 class BaseActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_base)
+
+        shouldShowOnBoarding()
+    }
+
+    private fun shouldShowOnBoarding() {
+        val firstRunHelper = FirstRunHelper(application)
+        if (firstRunHelper.isFirstRun()) {
+            showOnBoard()
+        }
+    }
+
+    private fun showOnBoard() {
+        val intent = Intent(this, WelcomeActivity::class.java)
+        startActivity(intent)
+        finish()
     }
 }
