93d2544646c00005e11a0cf0653a7a7795f6325d
Pushkar Anand <anandpushkar088@gmail.com>
2019-10-29 23:32:15 +0530

    fixed urls.

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -2,29 +2,20 @@
 #include <string>
 #include "Keys.h"
 
-extern "C" JNIEXPORT jstring JNICALL
-Java_me_pushkaranand_copymeanx_ui_MainActivity_appId(
-        JNIEnv *env,
-        jobject pThis
-) {
-    char APP_ID[] = OXFORD_APP_ID;
-    return env->NewStringUTF(APP_ID);
+const char *getCollegiateAPIKey() {
+    return WEBSTER_COLLEGIATE_KEY;
 }
 
-const char *getAppID() {
-    return OXFORD_APP_ID;
-}
-
-const char *getAppKey() {
-    return OXFORD_APP_KEY;
+const char *getLearnersAPIKey() {
+    return WEBSTER_LEARNERS_KEY;
 }
 
 extern "C" JNIEXPORT jstring JNICALL
-Java_me_pushkaranand_copymean_data_DataRepository__getAppID(JNIEnv *env, jobject pthis) {
-    return env->NewStringUTF(getAppID());
+Java_me_pushkaranand_copymean_data_DataRepository__getCollegiateAPIKey(JNIEnv *env, jobject pthis) {
+    return env->NewStringUTF(getCollegiateAPIKey());
 }
 
 extern "C" JNIEXPORT jstring JNICALL
-Java_me_pushkaranand_copymean_data_DataRepository__getAppKey(JNIEnv *env, jobject pthis) {
-    return env->NewStringUTF(getAppKey());
+Java_me_pushkaranand_copymean_data_DataRepository__getLearnersAPIKey(JNIEnv *env, jobject pthis) {
+    return env->NewStringUTF(getLearnersAPIKey());
 }
