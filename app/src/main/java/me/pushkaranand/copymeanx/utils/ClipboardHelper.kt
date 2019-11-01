d2f5aa05b9defbd11ac6672fa56309d33253bb77
Pushkar Anand <anandpushkar088@gmail.com>
2019-11-01 11:28:43 +0530

    dummy method to handle copied text.

--- app/src/main/java/me/pushkaranand/copymeanx/utils/ClipboardHelper.kt
+++ app/src/main/java/me/pushkaranand/copymeanx/utils/ClipboardHelper.kt
@@ -7,16 +7,24 @@ object ClipboardHelper {
 
     var lastCopiedData = ""
     private const val TAG = "CLIPBOARD HELPER"
-    
+
     fun handleClipData(clipData: ClipData?) {
         if (clipData != null) {
             val copiedText = clipData.getItemAt(0).text.toString().trim()
             if (copiedText.isNotEmpty() && lastCopiedData != copiedText) {
                 Log.d(TAG, "Copied: $copiedText")
                 lastCopiedData = copiedText
+                handleCopiedText(copiedText)
             }
         } else {
             Log.d(TAG, "clipData is null")
+        }
+    }
+
+    private fun handleCopiedText(copiedText: String) {
+        val regex = Regex("[a-zA-Z]+")
+        if (copiedText.matches(regex)) {
+            // TODO get meaning an display bubble.
         }
     }
 
