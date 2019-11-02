f1d0fa9cf6f0ae1e33819a02ca87389442ee6b57
Pushkar Anand <anandpushkar088@gmail.com>
2019-11-03 02:33:49 +0530

    added dailySwitch methods.

--- app/src/main/java/me/pushkaranand/copymeanx/ui/fragments/settings/SettingsFragment.kt
+++ app/src/main/java/me/pushkaranand/copymeanx/ui/fragments/settings/SettingsFragment.kt
@@ -9,16 +9,23 @@ import androidx.preference.PreferenceFragmentCompat
 import androidx.preference.SwitchPreference
 import kotlinx.android.synthetic.main.activity_base.*
 import me.pushkaranand.copymeanx.R
+import me.pushkaranand.copymeanx.utils.AlarmHelper
 import me.pushkaranand.copymeanx.utils.ClipboardHelper
 
 class SettingsFragment : PreferenceFragmentCompat() {
 
     private lateinit var clipMonitorSwitch: SwitchPreference
+    private lateinit var dailyNotificationSwitch: SwitchPreference
 
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
         setPreferencesFromResource(R.xml.pref_settings, rootKey)
-        clipMonitorSwitch = findPreference(getString(R.string.key_clipboard_monitor))!!
+        initVars()
         setupListeners()
+    }
+
+    private fun initVars() {
+        clipMonitorSwitch = findPreference(getString(R.string.key_clipboard_monitor))!!
+        dailyNotificationSwitch = findPreference(getString(R.string.key_daily_word_notify))!!
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -30,6 +37,7 @@ class SettingsFragment : PreferenceFragmentCompat() {
 
     private fun setupListeners() {
         setupClipMonitorSwitchListener()
+        setupDailyNotificationSwitchListener()
     }
 
     private fun setupClipMonitorSwitchListener() {
@@ -39,6 +47,19 @@ class SettingsFragment : PreferenceFragmentCompat() {
                 ClipboardHelper.startClipboardMonitoring(requireContext())
             } else {
                 ClipboardHelper.stopClipboardMonitoring(requireContext())
+            }
+            true
+        }
+    }
+
+    private fun setupDailyNotificationSwitchListener() {
+        val alarmHelper = AlarmHelper(requireContext())
+        dailyNotificationSwitch.setOnPreferenceChangeListener { _, newValue ->
+            val state = newValue as Boolean
+            if (state) {
+                alarmHelper.setDailyAlarm()
+            } else {
+                alarmHelper.disableDailyAlarm()
             }
             true
         }
